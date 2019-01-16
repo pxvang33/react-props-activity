9dfaed563ca7d5f6273c68388599d10f1706d3a4
Peng Xue Vang <pxvang33@gmail.com>
2019-01-16 15:12:39 -0600

    header component works

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Header extends Component {
+    render() {
+        return(
+            <header className="App-header">
+                <h2>React Components</h2>
+            </header>
+        ); // end return
+    } // end render
+} // end class
+
+export default Header;
